6bb031d1046ff455709770b47e0ba2c1efe62256
jinboMa <596756194@qq.com>
2018-02-04 15:40:17 +0800

    add react-router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        init my app.
-      </div>
+      <Router>
+        <div className="App">
+          init my app.
+        </div>
+      </Router>
     )
   }
 }
